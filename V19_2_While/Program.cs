37876db27d93e8850fd8fafa92f8d5f46755bb73
Ezequiel Cornec <Ezequiel.Cornec@Alumnos.uai.edu.ar>
2020-08-03 05:51:41 -0300

    Agregar archivos de proyecto.

---
+++ V19_2_While/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace V19_2_While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random Numero = new Random();
+
+            int NumeroRandom = Numero.Next(0, 100);
+
+            int Intentos = 0;
+
+            Console.WriteLine("¡Intente adivinar el número entre 0 y 100!");
+
+            int Intento = Int32.Parse(Console.ReadLine());
+
+            while(Intento != NumeroRandom)
+            {
+                Intentos++;
+                if(Intento > NumeroRandom)
+                {
+                    Console.WriteLine("El número ingresado es mayor.");
+                }
+                else
+                {
+                    Console.WriteLine("El número ingresado es menor.");
+                }
+                Console.WriteLine("¡Intente nuevamente adivinar el número entre 0 y 100!");
+
+                Intento = Int32.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine($"¡Felicitaciones! El número era {NumeroRandom}. Usted tuvo {Intentos} intentos fallidos.");
+        }
+    }
+}
